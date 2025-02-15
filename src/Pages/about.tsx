c2140ece1dd76829af2d7f08751d69e22e891168
pawan100722 <pawan100722@gmail.com>
2025-02-15 18:28:23 +0530

    added about page

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -1,43 +1,43 @@
-// import '../CSS/about.css';
+import '../Styles/about.css';
 import { AboutDetail } from '../Components/about-detail';
 
 export const About=()=>{
   return (
-    <div className="about-main-container">
-      <div className="summary-container">
-        <p className="summary">
+    <div className="about-page-main-container">
+      <div className="about-summary-container">
+        <p className="about-short-summary">
           I am Pawan Kumar, a software engineer with over 2 years of experience
           in web development. Based in Bareilly, Uttar Pradesh, India, I hold a
           BCA and an MCA degree. My expertise lies in both backend and frontend
           technologies, and I have worked on several projects that involve
           creating scalable and efficient web applications.
         </p>
-        <ul className="technical-skills">
+        <ul className="about-tech-skills-container">
           <li>
-            <span className="heading">Backend Development</span>: I have
-            experience using Node.js and Express.js to build RESTful APIs, work
-            with databases (such as MongoDB and MySQL), and implement features
-            like authentication and authorization.
+            <span className="about-skill-heading">Backend Development</span>: I
+            have experience using Node.js and Express.js to build RESTful APIs,
+            work with databases (such as MongoDB and MySQL), and implement
+            features like authentication and authorization.
           </li>
           <li>
-            <span className="heading">Frontend Development</span>: I am skilled
-            in React.js, creating dynamic user interfaces, managing state with
-            Redux, and ensuring responsive designs with HTML, CSS, and
-            JavaScript.
+            <span className="about-skill-heading">Frontend Development</span>: I
+            am skilled in React.js, creating dynamic user interfaces, managing
+            state with Redux, and ensuring responsive designs with HTML, CSS,
+            and JavaScript.
           </li>
         </ul>
-        <p className="skills-container">
-          <h1 className="about-skill-heading">Key Skills</h1>
+        <p className="about-tech-skills-container">
+          <p className="about-skill-heading">Key Skills</p>
           <ul>
             <li>
-              <span className="heading2">Backend:</span> Node.js, Nest.js, REST
+              <span className="about-skill-heading2">Backend:</span> Node.js, Nest.js, REST
               APIs, MongoDB, MySQL
             </li>
             <li>
-              <span className="heading2">Frontend:</span> React.js, HTML5, CSS3
+              <span className="about-skill-heading2">Frontend:</span> React.js, HTML5, CSS3
             </li>
             <li>
-              <span className="heading2">Version Control:</span> Git, GitHub
+              <span className="about-skill-heading2">Version Control:</span> Git, GitHub
             </li>
           </ul>
           I am passionate about building both backend systems and user-friendly
@@ -45,8 +45,8 @@ export const About=()=>{
         </p>
       </div>
 
-      <div className='about-contact-container'>
-        <h1>Contact Information</h1>
+      <div className="about-contact-container">
+        <h1 className='about-skill-heading'>Contact Information</h1>
         <AboutDetail title="Email" detail="pawan100722@gmail.com" />
         <AboutDetail title="Phone" detail="8433004706" />
         {/* <AboutDetail title="Email" detail="pawan100722@gmail.com" /> */}
