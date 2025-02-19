4862fedf7bf2a4d77e52a908541b75fefc659a9c
pawan100722 <pawan100722@gmail.com>
2025-02-19 16:17:21 +0530

    few improvements in CSS

--- src/Pages/homepage.tsx
+++ src/Pages/homepage.tsx
@@ -6,11 +6,11 @@ export const Homepage=()=>{
   return (
     <>
       <div className="homepage-main-container">
+        <div className="homepage-items-container">
           <img className='homepage-image' src={myImage} />
-        <div className="text-container">
           <p className='intro-text'>
-            Hi my name is Pawan Kumar. I am a <span className='highlighted-text'>Software Engineer</span>. I
-            am skilled in <span className='highlighted-text'>Web Development</span>
+            Hi my name is Pawan Kumar. I am a <span className='highlighted-text custom-words'>Software Engineer</span>. I
+            am skilled in <span className='highlighted-text animated-text'>Web Development</span>
           </p>
         </div>
       </div>
