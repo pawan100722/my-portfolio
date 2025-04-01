85bc1d974f97b1123a5b8c57bda6a1392408261f
pawan100722 <pawan100722@gmail.com>
2025-04-01 16:42:22 +0530

    added few improvements

--- src/Components/project.tsx
+++ src/Components/project.tsx
@@ -1,12 +1,12 @@
-export const Project = ({ image, title, description, projectURL }: ProjectDTO) => {
-  const handleParentGrow = () => {
-    addClass({ idParam: "parent", classNameParam: "increase-width" });
-    removeClass({ idParam: "parent", classNameParam: "decrease-width" });
+export const Project = ({ id, image, title, description, projectURL,animation }: ProjectDTO) => {
+  const handleParentGrow = (id: string) => {
+    addClass({ idParam: id, classNameParam: "increase-width" });
+    removeClass({ idParam: id, classNameParam: "decrease-width" });
   };
 
-  const handleParentShrink = () => {
-    addClass({ idParam: "parent", classNameParam: "decrease-width" });
-    removeClass({ idParam: "parent", classNameParam: "increase-width" });
+  const handleParentShrink = (id: string) => {
+    addClass({ idParam: id, classNameParam: "decrease-width" });
+    removeClass({ idParam: id, classNameParam: "increase-width" });
   };
 
   const addClass = ({ idParam, classNameParam }: AddClassParamDTO) => {
@@ -20,26 +20,28 @@ export const Project = ({ image, title, description, projectURL }: ProjectDTO) =
   };
 
   return (
-    <div className="project-component-container left-animation" id="parent">
+    <div
+      className={`project-component-container ${animation}`}
+      id={id}
+      onMouseEnter={() => handleParentGrow(id)}
+      onMouseLeave={() => handleParentShrink(id)}
+    >
       <a href={projectURL} target="blank">
-        <img
-          src={image}
-          alt={`${title}-project-image`}
-          onMouseEnter={handleParentGrow}
-          onMouseLeave={handleParentShrink}
-        />
+        <img src={image} alt={`${title}-project-image`} />
+        <h1>{title}</h1>
+        <p>{description}</p>
       </a>
-      <h1>{title}</h1>
-      <p>{description}</p>
     </div>
   );
 };
 
 interface ProjectDTO {
+  id: string;
   image: string;
   title: string;
   description: string;
   projectURL: string;
+  animation: string
 }
 
 interface AddClassParamDTO {
--- src/Pages/my-projects.tsx
+++ src/Pages/my-projects.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import '../Styles/projects.css';
 import { Project } from "../Components/project.tsx";
 import whatsappWebCloneImage from "../Images/projects/whatsapp-web-clone.png";
+import amazonCloneImage from '../Images/projects/amazon-clone.png'
 
 export const MyProjects=()=>{
   useEffect(()=>{
@@ -12,10 +13,20 @@ export const MyProjects=()=>{
     <div className="projects-page-container">
       <h1 className="projects-page-main-heading">My Projects</h1>
       <Project
+      id='project1'
         image={whatsappWebCloneImage}
         projectURL="https://pawan100722.github.io/whatsapp-web-clone/"
         title="Whatsapp Web Clone"
         description="This project is a clone/sample of the web version of the whatsapp app"
+        animation="left-animation"
+      />
+      <Project
+      id='project2'
+        image={amazonCloneImage}
+        projectURL="https://pawan100722.github.io/amazon-clone/"
+        title="Amazon Clone"
+        description="This project is a clone/sample of the amazon shopping app"
+        animation='right-animation'
       />
     </div>
   );
